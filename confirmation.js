a70c2ab19f0154dbee5685c5af123e70a2fbb80e
Mary Judi Kavitha Karunakaran <kavitha.rijesh@gmail.com>
2024-07-04 20:26:21 +0200

    Shipping page styling

--- confirmation.js
+++ confirmation.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cartData = localStorage.getItem('cart');
     if (cartData) {
-        const cart = JSON.parse(cartData);
-        const bookList = document.getElementById('book-list');
+        let cart = JSON.parse(cartData);
+        let bookList = document.getElementById('book-list');
 
         cart.forEach(book => {
-            const listItem = document.createElement('li');
+            let listItem = document.createElement('li');
             listItem.textContent = `${book.title} by ${book.author} - Quantity: ${book.quantity}`;
             bookList.appendChild(listItem);
         });
@@ -13,8 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
         // Clear the cart data after displaying it
         localStorage.removeItem('cart');
     } else {
-        const bookList = document.getElementById('book-list');
-        const listItem = document.createElement('li');
+        let bookList = document.getElementById('book-list');
+        let listItem = document.createElement('li');
         listItem.textContent = 'No books were found in your order.';
         bookList.appendChild(listItem);
     }
